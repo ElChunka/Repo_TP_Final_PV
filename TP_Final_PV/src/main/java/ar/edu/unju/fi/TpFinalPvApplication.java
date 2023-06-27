cbff742735f2422cb341ef4cc7f1f9bb3e94885f
ElChunka <lucas05091987@gmail.com>
2023-06-26 21:26:26 -0300

    Proyecto Trabajo Practico Final Programacion Visual

---
+++ TP_Final_PV/src/main/java/ar/edu/unju/fi/TpFinalPvApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalPvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalPvApplication.class, args);
+	}
+
+}
