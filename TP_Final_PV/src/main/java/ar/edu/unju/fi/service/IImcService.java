21a2dd21d395a26ca56c8f682fabb8b8967886d7
Lucas Andres Sanchez Rojas <82423477+ElChunka@users.noreply.github.com>
2023-07-10 17:00:12 -0300

    Update IImcService.java
    Se agrega comentarios en el codigo de IImcService.

--- TP_Final_PV/src/main/java/ar/edu/unju/fi/service/IImcService.java
+++ TP_Final_PV/src/main/java/ar/edu/unju/fi/service/IImcService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import ar.edu.unju.fi.entity.IndiceMasaCorporal;
 
 public interface IImcService {
-	
+
+	// Obtiene lista IMC (Indice de Masa Corporal) de un usuario por su ID.
 	List<IndiceMasaCorporal> listarImcPorUsuario(Long usuarioId);
+
+	//IMC (Indice de Masa Corporal)
 	void calcularIMC(Double peso, int codigo);
 
 }
