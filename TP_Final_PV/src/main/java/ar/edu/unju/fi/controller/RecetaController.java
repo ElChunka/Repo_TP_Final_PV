1021884b606f3c4f19d2c3614ce283b7d9572015
Mauricio Miranda <129995731+MauricioMiranda6030@users.noreply.github.com>
2023-07-10 17:33:48 -0300

    Update RecetaController.java
    Se agregaron comentarios

--- TP_Final_PV/src/main/java/ar/edu/unju/fi/controller/RecetaController.java
+++ TP_Final_PV/src/main/java/ar/edu/unju/fi/controller/RecetaController.java
@@ -47,23 +47,23 @@ public class RecetaController {
 	
 	@ModelAttribute("categorias")
     public List<String> getCategorias() {
-        return Arrays.asList("Carnes", "Bebidas", "Dulces", "Ensaladas", "Legumbres y Cereales", "Pescados", "Pan", "Sopas y Cremas");
+        return Arrays.asList("Carnes", "Bebidas", "Dulces", "Ensaladas", "Legumbres y Cereales", "Pescados", "Pan", "Sopas y Cremas"); //Categorias para el filtrado
     }
 	
 	@GetMapping("/lista")
 	public String getRecetasPage(Model model) {
-		model.addAttribute("recetas", recetaService.getRecetas());
+		model.addAttribute("recetas", recetaService.getRecetas()); //lista todas las recetas
 		return "recetasql";
 	}
 	@GetMapping("/lista/{categoria}")
-	public String getRecetasCategoriasPage(Model model, @PathVariable(value = "categoria") String categoria) {
-		model.addAttribute("recetas", recetaService.getRecetaCategoria(categoria));
+	public String getRecetasCategoriasPage(Model model, @PathVariable(value = "categoria") String categoria) { 
+		model.addAttribute("recetas", recetaService.getRecetaCategoria(categoria)); //lista recetas por categoria
 		return "recetasql";
 	}
 	
 	@GetMapping("/preparacion/{id}")
 	public ModelAndView getRecetapage(@PathVariable("id") Long id) {
-		ModelAndView modelView = new ModelAndView("mostrar_receta");
+		ModelAndView modelView = new ModelAndView("mostrar_receta"); //Devuelve la receta con sus ingredientes y su preparación con id pasada por url
 		receta = recetaService.getBy(id);
 		modelView.addObject("receta", receta);
 		modelView.addObject("filename", receta.getImagen());
@@ -73,17 +73,17 @@ public class RecetaController {
 	
 	@GetMapping("/nueva-receta")
 	public String getRecetasFormPage(Model model) {
-		model.addAttribute("receta", recetaService.getReceta());
-		model.addAttribute("ingredientes", ingredienteService.getIngredientes());
+		model.addAttribute("receta", recetaService.getReceta()); //devuelve un entity receta vacio
+		model.addAttribute("ingredientes", ingredienteService.getIngredientes()); //devuelve los ingredientes para el formulario
 		return "nueva_receta";
 	}
 	
 	@PostMapping("/guardar")
-	public ModelAndView getGuardarNuevaRecetaPage(@Valid @ModelAttribute("receta") Receta receta, BindingResult result,
-			@RequestParam("file") MultipartFile image) throws Exception{
+	public ModelAndView getGuardarNuevaRecetaPage(@Valid @ModelAttribute("receta") Receta receta, BindingResult result, // Parametros para validacion de datos
+			@RequestParam("file") MultipartFile image) throws Exception{ // Parametros para pasar la imagen a txt
 		
 		ModelAndView modelView = new ModelAndView("recetasql");
-		if(result.hasErrors()) {
+		if(result.hasErrors()) { // Error devuelve al formulario de receta
 			modelView.setViewName("nueva_receta");
 			modelView.addObject("receta", receta);
 			modelView.addObject("ingredientes", ingredienteService.getIngredientes());
@@ -97,11 +97,11 @@ public class RecetaController {
 		}
 		
 		recetaService.guardar(receta);
-		modelView.addObject("recetas", recetaService.getRecetas());
+		modelView.addObject("recetas", recetaService.getRecetas()); //Devuelve las recetas 
 		return modelView;
 	}
 	
-	@GetMapping("/uploads/{filename}")
+	@GetMapping("/uploads/{filename}") //Pasaa las imagenes para guardarlas como txt
 	public ResponseEntity<Resource> goImage(@PathVariable String filename){
 		Resource resource = null;
 		try {
